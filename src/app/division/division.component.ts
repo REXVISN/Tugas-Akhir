2ccdd56644923abd3018b30fb8c672269789ec2a
Rexvisn <gilangcosner@gmail.com>
2022-01-18 15:38:57 +0700

    update

--- src/app/division/division.component.ts
+++ src/app/division/division.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Division } from '../model/division.model';
+import { DivisionService } from '../services/division.service';
 @Component({
   selector: 'app-division',
   templateUrl: './division.component.html',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DivisionComponent implements OnInit {
 
-  constructor() { }
+  divisions?:Division[];
+  currentDivision:Division={};
+  name='';
+  constructor(private divisionService:DivisionService) { }
 
   ngOnInit(): void {
+    this.retrieveDivision()
   }
 
+
+  retrieveDivision():void{
+    this.divisionService.getAll()
+    .subscribe(
+      data=>{
+        this.divisions=data;
+        console.log(data);
+
+      },
+      error=>{
+        console.log(error)
+      }
+    )
+  }
 }
